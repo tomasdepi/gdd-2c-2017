37b8395a7328bd566558153e995f45a7d81c6b2f
tomasdepi <tomas94depi@gmail.com>
2017-10-22 22:49:54 -0300

    repo devolucion

---
+++ src/PagoAgilFrba/Entities/Devolucion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagoAgilFrba.Entities
+{
+    public class Devolucion
+    {
+        public int id { get; set; }
+        public DateTime fecha { get; set; }
+        public string tipoEntidad { get; set; }
+        public int idEntidad { get; set; }
+        public string motivo { get; set; }
+    }
+}
---
+++ src/PagoAgilFrba/Repository/RepoDevolucion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagoAgilFrba.Repository
+{
+    public class RepoDevolucion : Repo
+    {
+        public void altaDevolucion(Entities.Devolucion devolucion)
+        {
+            var query = "INSERT INTO PIZZA.Devolucion(dev_tipoEntidad, dev_fecha, dev_motivo, dev_entidad)";
+            query += "VALUES (@tipoEntidad, @fecha, @motivo, @idEntidad)";
+
+            this.Command = new SqlCommand(query, this.Connector);
+
+            this.Command.Parameters.Add("@tipoEntidad", SqlDbType.VarChar).Value = devolucion.tipoEntidad;
+            this.Command.Parameters.Add("@fecha", SqlDbType.Date).Value = devolucion.fecha;
+            this.Command.Parameters.Add("@motivo", SqlDbType.VarChar).Value = devolucion.motivo;
+            this.Command.Parameters.Add("@idEntidad", SqlDbType.Int).Value = devolucion.idEntidad;
+
+            this.Connector.Open();
+            this.Command.ExecuteNonQuery();
+            this.Connector.Close();
+        }
+    }
+}
