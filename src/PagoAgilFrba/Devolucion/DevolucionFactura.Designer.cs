e70dc828b56a5ce2e5b4e6f99b205490b84a5eac
tomasdepi <tomas94depi@gmail.com>
2017-10-22 22:19:42 -0300

    ui devolucion

---
+++ src/PagoAgilFrba/Devolucion/DevolucionFactura.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace PagoAgilFrba.Devolucion
+{
+    partial class DevolucionFactura
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtFactura = new System.Windows.Forms.TextBox();
+            this.txtMotivo = new System.Windows.Forms.TextBox();
+            this.btnSeleccionarFactura = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Factura:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Motivo:";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(29, 111);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(95, 23);
+            this.btnAceptar.TabIndex = 2;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(162, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtFactura
+            // 
+            this.txtFactura.Location = new System.Drawing.Point(81, 19);
+            this.txtFactura.Name = "txtFactura";
+            this.txtFactura.ReadOnly = true;
+            this.txtFactura.Size = new System.Drawing.Size(100, 20);
+            this.txtFactura.TabIndex = 4;
+            // 
+            // txtMotivo
+            // 
+            this.txtMotivo.Location = new System.Drawing.Point(81, 60);
+            this.txtMotivo.Name = "txtMotivo";
+            this.txtMotivo.Size = new System.Drawing.Size(191, 20);
+            this.txtMotivo.TabIndex = 5;
+            // 
+            // btnSeleccionarFactura
+            // 
+            this.btnSeleccionarFactura.Location = new System.Drawing.Point(197, 17);
+            this.btnSeleccionarFactura.Name = "btnSeleccionarFactura";
+            this.btnSeleccionarFactura.Size = new System.Drawing.Size(75, 23);
+            this.btnSeleccionarFactura.TabIndex = 6;
+            this.btnSeleccionarFactura.Text = "Seleccionar";
+            this.btnSeleccionarFactura.UseVisualStyleBackColor = true;
+            // 
+            // DevolucionFactura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 153);
+            this.Controls.Add(this.btnSeleccionarFactura);
+            this.Controls.Add(this.txtMotivo);
+            this.Controls.Add(this.txtFactura);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "DevolucionFactura";
+            this.Text = "DevolucionFactura";
+            this.Load += new System.EventHandler(this.DevolucionFactura_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtFactura;
+        private System.Windows.Forms.TextBox txtMotivo;
+        private System.Windows.Forms.Button btnSeleccionarFactura;
+    }
+}
---
+++ src/PagoAgilFrba/Devolucion/DevolucionRendicion.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PagoAgilFrba.Devolucion
+{
+    partial class DevolucionRendicion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSeleccionarRendicion = new System.Windows.Forms.Button();
+            this.txtMotivo = new System.Windows.Forms.TextBox();
+            this.txtFactura = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSeleccionarRendicion
+            // 
+            this.btnSeleccionarRendicion.Location = new System.Drawing.Point(197, 16);
+            this.btnSeleccionarRendicion.Name = "btnSeleccionarRendicion";
+            this.btnSeleccionarRendicion.Size = new System.Drawing.Size(75, 23);
+            this.btnSeleccionarRendicion.TabIndex = 13;
+            this.btnSeleccionarRendicion.Text = "Seleccionar";
+            this.btnSeleccionarRendicion.UseVisualStyleBackColor = true;
+            // 
+            // txtMotivo
+            // 
+            this.txtMotivo.Location = new System.Drawing.Point(81, 59);
+            this.txtMotivo.Name = "txtMotivo";
+            this.txtMotivo.Size = new System.Drawing.Size(191, 20);
+            this.txtMotivo.TabIndex = 12;
+            // 
+            // txtFactura
+            // 
+            this.txtFactura.Location = new System.Drawing.Point(81, 18);
+            this.txtFactura.Name = "txtFactura";
+            this.txtFactura.ReadOnly = true;
+            this.txtFactura.Size = new System.Drawing.Size(100, 20);
+            this.txtFactura.TabIndex = 11;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(162, 110);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(95, 23);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(29, 110);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(95, 23);
+            this.btnAceptar.TabIndex = 9;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Motivo:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Rendicion";
+            // 
+            // DevolucionRendicion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 152);
+            this.Controls.Add(this.btnSeleccionarRendicion);
+            this.Controls.Add(this.txtMotivo);
+            this.Controls.Add(this.txtFactura);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "DevolucionRendicion";
+            this.Text = "DevolucionRendicion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSeleccionarRendicion;
+        private System.Windows.Forms.TextBox txtMotivo;
+        private System.Windows.Forms.TextBox txtFactura;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
